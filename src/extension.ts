be816146206d6985ee0eb0711ba7c6d9095fe39d
Nils Emmenegger <61921611+nils-emmenegger@users.noreply.github.com>
2023-07-22 17:54:30 -0400

    Add submit command and keybinding

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,13 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+// Spawn a terminal and run dmoj-submit in it
+function callDmojSubmit(fileName: string) {
+	let terminal = vscode.window.createTerminal("dmoj-submit");
+	terminal.show();
+	terminal.sendText(`dmoj-submit submit '${fileName}'`);
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -13,10 +20,29 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('dmoj-submit-vscode.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('dmoj-submit-vscode.submit', () => {
 		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from dmoj-submit-vscode!');
+
+		// try to get current active document
+		let activeDocument = vscode.window.activeTextEditor?.document;
+		if (activeDocument === undefined) {
+			return vscode.window.showErrorMessage("Could not get the current active document");
+		}
+		let fileName = activeDocument.fileName;
+		// check some error cases before trying to submit
+		if (activeDocument.isUntitled) {
+			// untitled file (has not been saved yet)
+			vscode.window.showErrorMessage("File cannot be submitted because it is untitled");
+		} else if (activeDocument.isDirty) {
+			// unsaved changes
+			vscode.window.showWarningMessage("File has unsaved changes. Submit anyway?", "Yes", "No").then((res) => {
+				if (res === "Yes") {
+					callDmojSubmit(fileName);
+				}
+			});
+		} else {
+			callDmojSubmit(fileName);
+		}
 	});
 
 	context.subscriptions.push(disposable);
